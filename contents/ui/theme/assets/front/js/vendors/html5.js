8e18d69cb28e09862d0beee1c6ed0f9a49096dfe
Jitesh Singhvi <singhvi.jitesh@gmail.com>
2018-09-10 06:30:11 +0530

    WP Static HTML Output plugin: 2018-09-10 01:00:10

--- contents/ui/theme/assets/front/js/vendors/html5.js
+++ contents/ui/theme/assets/front/js/vendors/html5.js
@@ -5,5 +5,5 @@
 <h1>Not Found</h1>
 <p>The requested URL /contents/ui/theme/assets/front/js/vendors/html5.js was not found on this server.</p>
 <hr>
-<address>Apache/2.4.29 (Ubuntu) Server at localhost/undefined Port 80</address>
+<address>Apache/2.4.29 (Ubuntu) Server at jiteshsinghvi.me Port 80</address>
 </body></html>
